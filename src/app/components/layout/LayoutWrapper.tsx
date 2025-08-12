44f7c9abb93dc17adc5c0d97acd2fd66fbed5207
Faizan <faizanshamsi2017@gmail.com>
2025-08-12 23:56:07 +0500

    sidebar collapsible

--- src/app/components/layout/LayoutWrapper.tsx
+++ src/app/components/layout/LayoutWrapper.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client"
+import React, {useState} from "react";
 import Header from "./Header";
 import { SideBar } from "./Sidebar";
 import NavigationLoader from "../loader/NavigationLoader";
@@ -8,6 +9,8 @@ interface LayoutWrapperProps {
 }
  
 const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
+  const [isCollapsed, setIsCollapsed] = useState(false);
+
   return (
     <>
       <div className="flex flex-col h-screen overflow-hidden">
@@ -18,12 +21,14 @@ const LayoutWrapper: React.FC<LayoutWrapperProps> = ({ children }) => {
         {/* Main body (Sidebar + Page Content) */}
         <div className="flex flex-1 overflow-hidden">
           {/* Sidebar */}
-          <aside className="w-[26.8rem] shrink-0 overflow-y-auto border-r bg-white">
-            <SideBar />
-          </aside>
+           <aside
+          className={`${isCollapsed ? "w-20" : "w-[26.8rem]"} shrink-0 overflow-y-auto border-r bg-white transition-all duration-300`}
+        >
+          <SideBar isCollapsed={isCollapsed} setIsCollapsed={setIsCollapsed} />
+        </aside>
  
           {/* Main content with scroll */}
-          <main className="flex-1 overflow-y-auto bg-[var(--store-bg)] mt-20 ">
+          <main className="flex-1 pl-[5rem] overflow-y-auto bg-[var(--store-bg)] mt-20 ">
             {children}
           </main>
         </div>
