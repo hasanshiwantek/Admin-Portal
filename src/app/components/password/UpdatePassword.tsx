9c58a13e5e812f7c531e5a56e1d7c7131bb0b0c2
Faizan <faizanshamsi2017@gmail.com>
2025-08-18 20:51:13 +0500

    change password updated

--- src/app/components/password/UpdatePassword.tsx
+++ src/app/components/password/UpdatePassword.tsx
@@ -5,25 +5,48 @@ import { Button } from '@/components/ui/button'
 import { useAppSelector, useAppDispatch } from '@/hooks/useReduxHooks';
 import { Eye, EyeOff } from "lucide-react";
 import { Label } from '@/components/ui/label';
+import { ChevronRight } from 'lucide-react';
 
+type Field = "old" | "new" | "confirm";
 
 const UpdatePassword = () => {
 
   const { loading, error } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
+  const toggleShowPassword = (field: Field) => {
+  setShow((s) => ({ ...s, [field]: !s[field] }));
+};
+
+const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const [show, setShow] = useState({
+  old: false,
+  new: false,
+  confirm: false,
+});
+
   const [formData, setFormData] = useState({
     oldPassword: "",
     newPassword: "",
     confirmPassword: "",
-    showPassword: false,
     });
 
   return (
-   <div className="flex flex-col min-h-screen mt-5 ">
+   <div className="flex flex-col min-h-screen ">
+
+     <div className="text-lg flex justify-start items-center font-light p-4 text-gray-500">
+          Home <ChevronRight className="h-5 w-6" />
+          <span className="!font-light !text-[var(--primary-color)]">
+            Change Password
+          </span>
+     </div>
 
       <form
         // onSubmit={""}
-        className=" p-6 rounded-md shadow-lg w-full border border-2 max-w-[50rem]"
+        className=" p-6 rounded-lg shadow-lg w-full border border-2 max-w-[50rem]"
       >
         <div className="flex flex-col ">
         <h1 className="!text-4xl mb-4 ">Change My Password (User Email)</h1>
@@ -33,59 +56,59 @@ const UpdatePassword = () => {
           <Label>Enter old password</Label>
           <Input
             name="oldPassword"
-            type="text"
+            type={show.old ? "text" : "password"}
             placeholder="Old Password"
             value={formData.oldPassword}
-            // onChange={handleChange}
+            onChange={handleChange}
             required
-            className="!max-w-full !text-2xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
+            className="!max-w-full !text-xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
           />
           <button
               type="button"
-            //   onClick={toggleShowPassword}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600"
+              onClick={() => toggleShowPassword("old")}
+              className="absolute right-5 top-[3.7rem] -translate-y-1/2 text-gray-600"
             >
-              {formData.showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              {show.old ? <EyeOff size={18} /> : <Eye size={18} />}
             </button>
           </div>
 
-          <div className="my-4">
+          <div className="my-4 relative">
           <Label>Enter new password</Label>
             <Input
               name="newPassword"
-              type={formData.showPassword ? "text" : "password"}
+              type={show.new ? "text" : "password"}
               placeholder=" New Password"
               value={formData.newPassword}
-            //   onChange={handleChange}
+              onChange={handleChange}
               required
-              className="!max-w-full !text-2xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
+              className="!max-w-full !text-xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
             />
             <button
               type="button"
-            //   onClick={toggleShowPassword}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600"
+              onClick={() => toggleShowPassword("new")}
+              className="absolute right-5 top-[3.7rem] -translate-y-1/2 text-gray-600"
             >
-              {formData.showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              {show.new ? <EyeOff size={18} /> : <Eye size={18} />}
             </button>
           </div>
 
-          <div className="my-4">
+          <div className="my-4 relative">
           <Label>Confirm new password</Label>
             <Input
-              name="password"
-              type={formData.showPassword ? "text" : "password"}
+              name="confirmPassword"
+              type={show.confirm ? "text" : "password"}
               placeholder=" Confirm Password"
               value={formData.confirmPassword}
-            //   onChange={handleChange}
+              onChange={handleChange}
               required
-              className="!max-w-full !text-2xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
+              className="!max-w-full !text-xl !font-light px-4 py-2 bg-blue-50 text-black placeholder:text-gray-500"
             />
             <button
               type="button"
-            //   onClick={toggleShowPassword}
-              className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-600"
+              onClick={() => toggleShowPassword("confirm")}
+              className="absolute right-5 top-[3.7rem] -translate-y-1/2 text-gray-600"
             >
-              {formData.showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+              {show.confirm ? <EyeOff size={18} /> : <Eye size={18} />}
             </button>
           </div>
 
@@ -94,7 +117,7 @@ const UpdatePassword = () => {
             variant="default"
             size="xxl"
             disabled={loading}
-            className="!w-full cursor-pointer my-3 !h-[4rem] bg-[#008696] rounded-4xl font-medium !text-2xl focus-within:ring-blue-200 focus-within:border-blue-200 transition hover:border-blue-200 hover:bg-[#3A426E] "
+            className="!w-full cursor-pointer my-3 !h-[4.5rem] bg-[#008696] rounded-[2rem] font-medium !text-2xl focus-within:ring-blue-200 focus-within:border-blue-200 transition hover:border-blue-200 hover:bg-[#3A426E] "
           >
             {loading ? "Updating..." : "Change Password"}
           </Button>
