055015bc9a57fc37759a4a3b438b446fa15ffdd0
Faizan <faizanshamsi2017@gmail.com>
2025-08-18 18:14:59 +0500

    studies page added

---
+++ src/app/(protected)/wellers/studies/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Studies from "@/app/components/wellers/studies/studies";
+
+const page = () => {
+  return (
+    <>
+        <div className="p-10">
+          <Studies />
+        </div>
+    </>
+  );
+};
+
+export default page;
--- src/const/sidebarData.ts
+++ src/const/sidebarData.ts
@@ -37,6 +37,7 @@ export const sidebarData = [
       { title: "Wam Backup", url: "/wellers/wam-backup" },
       { title: "Volunteer Info", url: "/wellers/volunteer-info" },
       { title: "Quick View", url: "/wellers/quick-view" },
+      { title: "Studies", url: "/wellers/studies" },
 
     ],
   },
