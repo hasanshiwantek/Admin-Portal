05351946b205ed8b78cf4b04fdb8338f8b6597cf
hasanshiwantek <hasan.shiwantek@gmail.com>
2025-09-05 21:34:28 +0500

    Integrated Get by ID weller API

--- src/app/components/administrative/EditWellers/EditWellers.tsx
+++ src/app/components/administrative/EditWellers/EditWellers.tsx
@@ -1,45 +1,46 @@
-"use client"
-import React,{useState} from 'react'
-import WellerInfo from './WellerInfo'
-import SelectWeller from './SelectWeller'
+"use client";
+import React, { useState } from "react";
+import WellerInfo from "./WellerInfo";
+import SelectWeller from "./SelectWeller";
 import Spinner from "../../loader/Spinner";
+import { useAppSelector } from "@/hooks/useReduxHooks";
 const EditWellers = () => {
   const [selectedWeller, setSelectedWeller] = useState<any | null>(null);
-    const [loadingWeller, setLoadingWeller] = useState(false);
-    const handleWellerSelect = (weller: any) => {
-      setLoadingWeller(true);
-      // Simulate slight loading delay
-      setTimeout(() => {
-        setSelectedWeller(weller);
-        setLoadingWeller(false);
-      }, 500); // 0.5s delay
-    };
+  const [loadingWeller, setLoadingWeller] = useState(false);
+  const handleWellerSelect = (weller: any) => {
+    setLoadingWeller(true);
+    // Simulate slight loading delay
+    setTimeout(() => {
+      setSelectedWeller(weller);
+      setLoadingWeller(false);
+    }, 500); // 0.5s delay
+  };
   return (
     <>
-    <div className="flex flex-col gap-5">
-      <SelectWeller
-        selectedWeller={selectedWeller}
-       setSelectedWeller={handleWellerSelect}
-      />
-         <div className="flex-1">
-                {loadingWeller ? (
-                  <div className="flex justify-center items-center h-[300px]">
-                    <Spinner />
-                  </div>
-                ) : selectedWeller ? (
-                  <WellerInfo
-                    selectedWeller={selectedWeller}
-                    setSelectedWeller={setSelectedWeller}
-                  />
-                ) : (
-                  <div className="text-gray-500 text-center mt-10">
-                    Select a weller to view their details.
-                  </div>
-                )}
-              </div>
-    </div>
+      <div className="flex flex-col gap-5">
+        <SelectWeller
+          selectedWeller={selectedWeller}
+          setSelectedWeller={handleWellerSelect}
+        />
+        <div className="flex-1">
+          {loadingWeller ? (
+            <div className="flex justify-center items-center h-[300px]">
+              <Spinner />
+            </div>
+          ) : selectedWeller ? (
+            <WellerInfo
+              selectedWeller={selectedWeller}
+              setSelectedWeller={setSelectedWeller}
+            />
+          ) : (
+            <div className="text-gray-500 text-center mt-10">
+              Select a weller to view their details.
+            </div>
+          )}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default EditWellers
+export default EditWellers;
